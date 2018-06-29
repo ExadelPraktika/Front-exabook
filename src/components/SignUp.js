be89f82a2a4602b258624763bfaf31fc115e564c
Deivydas Kinderis <kalted1997@gmail.com>
2018-06-29 10:59:13 +0300

    small fixes

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,96 +1,96 @@
-import React, { Component } from 'react'
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+
+const styles = {
+  card: {
+    minWidth: 275,
+    maxWidth: 300,
+    textAlign: "center"
+  }
+};
 
 class SignUp extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          isLoading: false,
-          userName: "",
-          email: "",
-          password: "",
-          confirmPassword: "",
-          confirmationCode: "",
-          newUser: null
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-      }
-      validateForm() {
-        return (
-          this.state.email.length > 0 &&
-          this.state.password.length > 0 &&
-          this.state.password === this.state.confirmPassword
-        );
-      }
-    
-      validateConfirmationForm() {
-        return this.state.confirmationCode.length > 0;
-      }
-    
-      handleChange(e) {
-        this.setState({
-          [e.target.id]: e.target.value,
-        })
-      }
-    
-      handleSubmit(e) {
-        e.preventDefault();
-    
-        this.setState({ isLoading: true });
-    
-        this.setState({ newUser: "test" });
-    
-        this.setState({ isLoading: false });
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      confirmationCode: "",
+      newUser: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  validateForm() {
+    return (
+      this.state.email.length > 0 &&
+      this.state.password.length > 0 &&
+      this.state.password === this.state.confirmPassword
+    );
+  }
 
-        alert(this.state.email);
-      }
-      
-    render(){
-        return (
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({ newUser: "test" });
+  }
+
+  render() {
+    return (
+      <Card className="container" style={styles.card}>
         <form onSubmit={this.handleSubmit}>
-            <Input 
+          <Input
             id="userName"
+            label="User Name"
             value={this.state.userName}
             onChange={this.handleChange}
             placeholder="Username"
-            />
-            <br />
-            <br />
-            <Input 
+          />
+          <br />
+          <br />
+          <Input
             id="email"
             value={this.state.email}
             onChange={this.handleChange}
             placeholder="email"
-            />
-            <br />
-            <br />
-            <Input
-            id="password" 
+          />
+          <br />
+          <br />
+          <Input
+            id="password"
             value={this.state.password}
             onChange={this.handleChange}
             type="password"
             placeholder="Password"
-            />
-            <br />
-            <br />
-            <Input
+          />
+          <br />
+          <br />
+          <Input
             id="confirmPassword"
             value={this.state.confirmPassword}
             onChange={this.handleChange}
             type="password"
             placeholder="Confirm Password"
-            />
-            <br />
-            <br />
-            <Button
-          disabled={!this.validateForm()}
-          type="submit">Signup</Button>
-        
+          />
+          <br />
+          <br />
+          <Button disabled={!this.validateForm()} type="submit">
+            Signup
+          </Button>
         </form>
-        )
-    }
+      </Card>
+    );
+  }
 }
 
 export default SignUp;
