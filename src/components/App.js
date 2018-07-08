3e9e361d7f01bb13b664f8161f4e7180c856fa15
edmarkou <edvardas.markevicius@gmail.com>
2018-07-09 01:20:58 +0300

    Topbar created

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import LogIn from './LogIn';
 import SignUp from './SignUp';
+import TopBar from './Topbar/Topbar';
 
 class App extends Component {
   render() {
     return(
       <div>
-      <h1>Exabook!</h1>
-      <LogIn />
-    </div>
+          <h1>Exabook!</h1>
+        <LogIn />
+      </div>
     )
   }
 }
