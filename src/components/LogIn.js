4916a49e35fe4cf7758dd1f834237f041f148c60
vinkky <nezinaugoblinas5@gmail.com>
2018-07-11 11:19:36 +0300

    added logout function, deleting redux auth state, deleting token, full login registration should work now

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Input from '@material-ui/core/Input';
 import Card from '@material-ui/core/Card';
+import { connect } from 'react-redux';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import { setCurrent, logoutUser } from '../actions/authActions'
 
 const styles = {
   card: {
@@ -22,12 +27,12 @@ const styles = {
 
 
 class LoginForm extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
-      formErrors: {email: '', password: ''},
+      formErrors: { email: '', password: '' },
       emailValid: false,
       passwordValid: false,
       formValid: false
@@ -42,71 +47,105 @@ class LoginForm extends Component {
     let fieldValidationErrors = this.state.formErrors;
     let emailValid = this.state.emailValid;
     let passwordValid = this.state.passwordValid;
-    switch(fieldName) {
+    switch (fieldName) {
       case 'email':
         emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
         fieldValidationErrors.email = emailValid ? '' : ' is invalid';
         break;
       case 'password':
         passwordValid = value.length >= 6;
-        fieldValidationErrors.password = passwordValid ? '': ' is too short';
+        fieldValidationErrors.password = passwordValid ? '' : ' is too short';
         break;
       default:
         break;
     }
 
-    this.setState({formErrors: fieldValidationErrors,
-                    emailValid: emailValid,
-                    passwordValid: passwordValid
-                  }, this.validateForm);
-  }
-  
-  validateForm() {
-    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    this.setState({
+      formErrors: fieldValidationErrors,
+      emailValid: emailValid,
+      passwordValid: passwordValid
+    }, this.validateForm);
   }
 
-  handleChange (evt) {
+  validateForm() {
+    this.setState({ formValid: this.state.emailValid && this.state.passwordValid });
+  }
+
+  handleChange(evt) {
     const name = evt.target.name;
     const value = evt.target.value;
 
-    this.setState({ [name]:value }, () => { this.validateField(name, value) });
+    this.setState({ [name]: value }, () => { this.validateField(name, value) });
   }
 
   handleSubmit(event) {
     //event.preventDefault()
     axios
-        .post('http://localhost:5000/api/users/login', {
-            email: this.state.email,
-            password: this.state.password
-        })
-        .then(response => {
-            console.log(response)
-        }).catch(error => {
-            console.log(error.response.data);
-        })
-}
+      .post('http://localhost:3001/users/signin', {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        console.log(response)
+        this.props.setCurrent(response.data.token)
+      }).catch(error => {
+        console.log(error.response.data);
+      })
+  }
+  responseFacebook = (response) => {
+    console.log(response);
+    const access_token = response.accessToken
+    axios
+      .post('http://localhost:3001/users/oauth/facebook', {
+        access_token
+      })
+      .then((response) => {
+        this.props.setCurrent(response.data.token)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-  render () {
+  responseGoogle = (response) => {
+    const access_token = response.Zi.access_token;
+    axios
+      .post('http://localhost:3001/users/oauth/google', {
+        access_token
+      })
+      .then((response) => {
+        this.props.setCurrent(response.data.token);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
+  render() {
     return (
       <div>
         <Card className="login-card" style={styles.card}>
           <CardContent>
             <FormControl component="fieldset">
-            <FormLabel>Login</FormLabel>
-              <Input 
+              <FormLabel>Login</FormLabel>
+              <Input
                 name="email"
                 placeholder="Email"
                 onChange={this.handleChange}
               />
-          
-              <Input 
+
+              <Input
                 name="password"
                 placeholder="Password"
                 onChange={this.handleChange}
                 type="password"
               />
 
-              <Button 
+              <Button
                 style={styles.button}
                 variant="contained"
                 onClick={this.handleSubmit}
@@ -115,6 +154,22 @@ class LoginForm extends Component {
               >
                 Login
               </Button>
+              <GoogleLogin
+                clientId="890644813294-bvuq6cf7lsilohneqvov28oi60sfdmig.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={response => this.responseGoogle(response)}
+                onFailure={response => this.responseGoogle(response)}
+              />
+              <FacebookLogin
+                appId="485850475180066"
+                autoLoad={false}
+                fields="name,email,picture"
+                //onClick={componentClicked}
+                callback={response => this.responseFacebook(response)}
+              />
+            <Button onClick={this.onLogoutClick.bind(this)} >
+            Logout
+          </Button>
             </FormControl>
           </CardContent>
         </Card>
@@ -123,4 +178,12 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+LoginForm.propTypes = {
+  setCurrent: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { setCurrent, logoutUser })(LoginForm);
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import GoogleLogin from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
-import jwt_decode from 'jwt-decode';
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import axios from "axios";
 import PropTypes from 'prop-types';
-import {setCurrent} from '../actions/authActions'
-import setAuthToken from '../utils/setAuthToken'
+import {setCurrent, logoutUser} from '../actions/authActions'
 
 
 const styles = {
@@ -64,10 +62,12 @@ class SignUp extends Component {
     };
     axios
       .post('http://localhost:3001/users/signup', {
-        newUser
+        email: this.state.email,
+        password: this.state.password
       })
-      .then(function (response) {
-        console.log(response);
+      .then((response) => {
+        console.log(response)
+        //this.props.setCurrent(response.data.token);
       })
       .catch(function (error) {
         console.log(error);
@@ -77,20 +77,12 @@ class SignUp extends Component {
   responseFacebook = (response) => {
     console.log(response);
     const access_token = response.accessToken
-    
     axios
       .post('http://localhost:3001/users/oauth/facebook', {
         access_token
       })
       .then((response) => {
-        // Save to localStorage
-        localStorage.setItem('jwtToken', response.data.token);
-        console.log(response);
-        // Set token to Auth header
-        setAuthToken(response.data.token);
-        const decoded = jwt_decode(response.data.token);
         this.props.setCurrent(response.data.token)
-        console.log(decoded);
       })
       .catch(function (error) {
         console.log(error);
@@ -98,33 +90,27 @@ class SignUp extends Component {
   }
 
   responseGoogle = (response) => {
-    console.log('google', response);
     const access_token = response.Zi.access_token;
     axios
       .post('http://localhost:3001/users/oauth/google', {
         access_token
       })
       .then((response) =>{
-        console.log('google', response);
-        // Save to localStorage
-        localStorage.setItem('jwtToken', response.data.token);
-        // Set token to Auth header
-        setAuthToken(response.data.token);
-        const decoded = jwt_decode(response.data.token);
         this.props.setCurrent(response.data.token);
-        console.log(decoded);
       })
       .catch(function (error) {
         console.log(error);
       });
   }
 
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
 
   render() {
-    const { user } = this.props.auth;
     return (
       <Card className="container" style={styles.card}>
-      {user ? user.sub : null}
         <form onSubmit={this.handleSubmit}>
           <GoogleLogin
             clientId="890644813294-bvuq6cf7lsilohneqvov28oi60sfdmig.apps.googleusercontent.com"
@@ -134,7 +120,7 @@ class SignUp extends Component {
           />
           <FacebookLogin
             appId="485850475180066"
-            autoLoad={true}
+            autoLoad={false}
             fields="name,email,picture"
             //onClick={componentClicked}
             callback={response => this.responseFacebook(response)}
@@ -177,6 +163,9 @@ class SignUp extends Component {
           <Button disabled={!this.validateForm()} type="submit">
             Signup
           </Button>
+          <Button onClick={this.onLogoutClick.bind(this)} >
+            Logout
+          </Button>
         </form>
       </Card>
     );
@@ -184,10 +173,11 @@ class SignUp extends Component {
 }
 SignUp.propTypes = {
   setCurrent: PropTypes.func.isRequired,
+  logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
 const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { setCurrent })(SignUp);
+export default connect(mapStateToProps, { setCurrent, logoutUser })(SignUp);
